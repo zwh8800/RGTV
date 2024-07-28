004c3a0f3e99fab6c55b7cbf8defb054927d32be
wastecat <6185988+zwh8800@users.noreply.github.com>
2024-07-28 21:40:50 +0800

    feat: use sdl.Main

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
-import app "github.com/zwh8800/rgbili/app"
+import (
+	"github.com/veandco/go-sdl2/sdl"
+
+	"github.com/zwh8800/rgbili/app"
+)
 
 func main() {
-	app, err := app.NewApp()
-	if err != nil {
-		panic(err)
-	}
-	defer app.Quit()
+	sdl.Main(func() {
+		sdl.Do(func() {
+			app, err := app.NewApp()
+			if err != nil {
+				panic(err)
+			}
+			defer app.Quit()
 
-	app.Run()
+			app.Run()
+		})
+	})
 }
