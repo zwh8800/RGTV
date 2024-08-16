41d638d4b0d33720e3ee8fdd1779e1bb3a4acb02
wastecat <6185988+zwh8800@users.noreply.github.com>
2024-08-16 23:51:29 +0800

    fix: Update build.sh

--- build.sh
+++ build.sh
@@ -3,14 +3,14 @@
 rm -rf build
 
 mkdir -p build/RGTV
-mkdir -p build/Img
+mkdir -p build/Imgs
 
 go build -o build/RGTV/RGTV
 cp ffmpeg_bin/ffmpeg-arm64 build/RGTV/ffmpeg
 cp sample_tv.json build/RGTV/tv.json
 
 cp RGTV.sh build/
-cp ./embeddata/RGTV.png build/Img
+cp ./embeddata/RGTV.png build/Imgs
 
 cd build
 zip -r RGTV.zip *
