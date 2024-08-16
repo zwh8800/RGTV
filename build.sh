906eb435fc606043d659f6961cc312ce2716ecfd
wastecat <6185988+zwh8800@users.noreply.github.com>
2024-08-16 16:11:54 +0000

    feat: add logo

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ cp ffmpeg_bin/ffmpeg-arm64 build/RGTV/ffmpeg
 cp sample_tv.json build/RGTV/tv.json
 
 cp RGTV.sh build/
-cp ./embeddata/RGTV.png build/Imgs
+cp ./RGTV.png build/Imgs
 
 cd build
 zip -r RGTV.zip *
